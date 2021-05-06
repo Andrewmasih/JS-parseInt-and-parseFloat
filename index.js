15f50abfd269e0844fc9dcbbbe5872ddd8afbaf1
Andrew Masih <armasih@outlook.com>
2021-05-06 22:45:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log(
+Number.parseInt("10"),
+Number.parseInt("3.14"),
+Number.parseInt("I am not an integer"));
+
+console.log(
+Number.parseFloat("10"),
+Number.parseFloat("3.14"),
+Number.parseFloat("I am not a float"));
