2e23b22e4875bbef5d0bec57e5b36a9378c19ba4
Andrew Masih <armasih@outlook.com>
2021-05-06 23:24:16 +0000

    added comments

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ Number.parseInt("10"),
 Number.parseInt("3.14"),
 Number.parseInt("I am not an integer"));
 
+/* The parseInt() function parses a string and returns an integer. */
+
 console.log(
 Number.parseFloat("10"),
 Number.parseFloat("3.14"),
 Number.parseFloat("I am not a float"));
+
+/* The parseFloat() function parses a string and returns a floating point number. */
